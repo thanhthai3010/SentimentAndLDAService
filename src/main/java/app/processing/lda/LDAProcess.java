8f5f7391fb66fcdc84781e02c5a155ed2a57b80e
thanhthai3010 <n.thanhthai3010@hotmail.com>
2015-11-25 10:26:16 +0700

    rename util pakage

--- src/main/java/app/processing/lda/LDAProcess.java
+++ src/main/java/app/processing/lda/LDAProcess.java
@@ -28,7 +28,7 @@ import org.apache.spark.mllib.linalg.Vectors;
 
 import app.utils.dto.InputDataForLDA;
 import scala.Tuple2;
-import utils.SparkUtil;
+import app.utils.spark.SparkUtil;
 
 /**
  * Implement Process of LDA Model
