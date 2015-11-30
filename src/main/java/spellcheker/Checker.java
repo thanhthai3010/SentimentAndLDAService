0c1187a85f425a07ce11b71113297bfef1a5c1a0
thanhthai3010 <n.thanhthai3010@hotmail.com>
2015-11-30 10:20:59 +0700

    Separate check spell và check emoticons

--- src/main/java/spellcheker/Checker.java
+++ src/main/java/spellcheker/Checker.java
@@ -15,7 +15,11 @@ public class Checker implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	static int index = 1;
+	
+	/**
+	 * String blank
+	 */
+	private static final String STRING_BLANK = "";
 
 	/**
 	 * default constructor
@@ -30,19 +34,33 @@ public class Checker implements Serializable {
 	 * @param String input sentences
 	 * @return result String after corrected
 	 */
-	public static String correct(String sentences) {
-		Map<String, String> dictCheckSpell = Dictionary.getDict().collectAsMap();
+	public static String correctSpell(String sentences) {
 		
-		for (Map.Entry<String, String> entry : dictCheckSpell.entrySet()) {
-			sentences = sentences.replaceAll("\\b" + entry.getKey() + "\\b", entry.getValue());
+		if (sentences != null && !STRING_BLANK.equals(sentences)) {
+			Map<String, String> dictCheckSpell = Dictionary.getDict().collectAsMap();
+			
+			for (Map.Entry<String, String> entry : dictCheckSpell.entrySet()) {
+				sentences = sentences.replaceAll("\\b" + entry.getKey() + "\\b", entry.getValue());
+			}
 		}
 		
-		Map<String, String> dictEmoticons = Dictionary.getDictEmoticons().collectAsMap();
-		
-		for (Map.Entry<String, String> emoticon : dictEmoticons.entrySet()) {
-			sentences = sentences.replaceAll("((?<=\\W)|^)\\Q" + emoticon.getKey() + "\\E((?=\\W)|$)", emoticon.getValue());
+		return sentences;
+	}
+	
+	/**
+	 * get the correct of sentences has emoticons
+	 * 
+	 * @param String input sentences
+	 * @return result String after corrected
+	 */
+	public static String correctEmoticons(String sentences){
+		if (sentences != null && !STRING_BLANK.equals(sentences)) {
+			Map<String, String> dictEmoticons = Dictionary.getDictEmoticons().collectAsMap();
+			
+			for (Map.Entry<String, String> emoticon : dictEmoticons.entrySet()) {
+				sentences = sentences.replaceAll("((?<=\\W)|^)\\Q" + emoticon.getKey() + "\\E((?=\\W)|$)", emoticon.getValue());
+			}
 		}
-		
 		return sentences;
 	}
 
