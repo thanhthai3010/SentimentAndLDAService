947aec32e44bb0a07b5391e8eb85bf914107ee20
thanhthai3010 <n.thanhthai3010@hotmail.com>
2015-12-07 22:00:08 +0700

    Change report data

--- src/main/java/app/utils/dto/ListReportData.java
+++ src/main/java/app/utils/dto/ListReportData.java
@@ -1,24 +1,48 @@
 package app.utils.dto;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.List;
 
 import com.google.gson.Gson;
 
-/**
- * List all of report object
- * @author thaint
- *
- */
-public class ListReportData extends ArrayList<ReportData> implements Serializable{
+public class ListReportData implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private ReportData statusData;
+	private List<ReportData> listCommentData;
+
+	public ListReportData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ListReportData(ReportData statusData, List<ReportData> listCommentData) {
+		super();
+		this.statusData = statusData;
+		this.listCommentData = listCommentData;
+	}
+
+	public ReportData getStatusData() {
+		return statusData;
+	}
+
+	public void setStatusData(ReportData statusData) {
+		this.statusData = statusData;
+	}
+
+	public List<ReportData> getListCommentData() {
+		return listCommentData;
+	}
+
+	public void setListCommentData(List<ReportData> listCommentData) {
+		this.listCommentData = listCommentData;
+	}
 	
-	public String pieDataToJson(){
-		return new Gson().toJson(this);
+	public static String toJson(List<ListReportData> input) {
+		return new Gson().toJson(input);
 	}
 
 }
