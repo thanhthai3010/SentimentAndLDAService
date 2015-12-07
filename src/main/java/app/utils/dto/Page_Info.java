5ac3ba4d26880d776673366b1e636ca4bab1dbcb
thanhthai3010 <n.thanhthai3010@hotmail.com>
2015-12-07 20:57:53 +0700

    Change name from PieData to ReportData

---
+++ src/main/java/app/utils/dto/Page_Info.java
@@ -0,0 +1,75 @@
+package app.utils.dto;
+
+import java.io.Serializable;
+
+/**
+ * TABLE PAGE_INFO
+ * @author thaint
+ *
+ */
+public class Page_Info implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * pageID of facebook page
+	 */
+	private Long pageID;
+	
+	/**
+	 * page name
+	 */
+	private String pageName;
+
+	/**
+	 * default constructor
+	 */
+	public Page_Info() {
+	}
+
+	/**
+	 * Constructor with parameter
+	 * @param pageID
+	 * @param pageName
+	 */
+	public Page_Info(Long pageID, String pageName) {
+		this.pageID = pageID;
+		this.pageName = pageName;
+	}
+
+	/**
+	 * get pageID
+	 * @return pageID
+	 */
+	public Long getPageID() {
+		return pageID;
+	}
+
+	/**
+	 * set pageID
+	 * @param pageID
+	 */
+	public void setPageID(Long pageID) {
+		this.pageID = pageID;
+	}
+
+	/**
+	 * get pageName
+	 * @return pageName
+	 */
+	public String getPageName() {
+		return pageName;
+	}
+
+	/**
+	 * set pageName
+	 * @param pageName
+	 */
+	public void setPageName(String pageName) {
+		this.pageName = pageName;
+	}
+
+}
